e172005973765d84c8f2f74ee7203ee8558c19b8
daiki nishimura0 <nishimura_dai@deltanet.co.jp>
2021-10-29 13:25:25 +0900

    feat: routerの導入による変更

--- src/App.vue
+++ src/App.vue
@@ -1,180 +1,79 @@
 <template>
-<v-container style="max-width:800px">
-    <v-system-bar
-      color="blue darken-2"
-      dark
-    >
-      <v-spacer></v-spacer>
-
-      <v-icon>mdi-window-minimize</v-icon>
-
-      <v-icon>mdi-window-maximize</v-icon>
-
-      <v-icon>mdi-close</v-icon>
-    </v-system-bar>
-
-    <v-app-bar
-      dark
-      color="blue"
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Todo</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-
-<v-card
-  class="mx-a"
-    outlined
->
-    <v-text-field 
-       v-model="newTask"
-        label="Todoを入力してね"
-        @keydown.enter="create"
-        filled 
-        outlined
-        single-line 
-        >
-      <template v-slot:append>
-        <v-fade-transition>
-          <v-icon
-            v-if="newTask"
-            @click="create"
-          >
-       mdi-pencil
-          </v-icon>
-        </v-fade-transition>
-      </template>
-    </v-text-field>
-
-    <h2 class="text-h4 success--text pl-4">
-      Tasks:
-      <v-fade-transition leave-absolute>
-        <span :key="`task-${task.length}`">
-          {{ task.length }}
-        </span>
-      </v-fade-transition>
-    </h2>
-
-    <v-divider class="mt-4"></v-divider>
-
-    <v-row
-      class="my-1"
-      align="center"
-    >
-      <strong class="mx-4 info--text text--darken-2">
-        未完了: {{ remainingTasks }}
-      </strong>
-
-      <strong class="mx-4 success--text text--darken-2">
-        完了: {{ completedTasks }}
-      </strong>
-
-      <v-spacer></v-spacer>
-
-      <v-progress-circular
-        :value="progress"
-        class="mr-4"
-      ></v-progress-circular>
-    </v-row>
-
-    <v-divider class="mb-4"></v-divider>
-
-    <v-card v-if="task.length > 0">
-      <v-slide-y-transition
-        class="py-0"
-        group
-      >
-      
-        <template v-for="(task, i) in task">
-          <v-divider
-            v-if="i !== 0"
-            :key="`${i}-divider`"
-          ></v-divider>
-
-          <v-list-item :key="`${i}-${task.text}`">
-            <v-list-item-action>
-              <v-checkbox
-                v-model="task.done"
-                :color="task.done && 'grey' || 'primary'"
-              >
-                <template v-slot:label>
-                  <div
-                    :class="task.done && 'grey--text' || 'primary--text'"
-                    class="ml-4"
-                    v-text="task.text"
-                  ></div>
-                </template>
-              </v-checkbox>
-            </v-list-item-action>
-
-            <v-spacer></v-spacer>
-
-            <v-scroll-x-transition>
-              <v-icon
-                v-if="task.done"
-                color="success"
-              >
-                mdi-check
-              </v-icon>
-            </v-scroll-x-transition>
-          </v-list-item>
-        </template>
-      </v-slide-y-transition>
-    </v-card>
-</v-card>
-    </v-container>
+  <div>
+    <router-link to="/">Home</router-link>
+    <v-spacer />
+    <router-link to="/add">Add</router-link>
+    <router-view />
+  </div>
 </template>
 
-<script lang='ts'>
-import { Component, Vue } from 'vue-property-decorator'
+<script lang="ts">
+// import axios from 'axios';
+// import { Component, Vue } from 'vue-property-decorator';
 
-interface  Task {
-  done: boolean;
-  text: any;
-}
+// const client = axios.create({
+//   baseURL: 'http://localhost:3000',
+//   headers: {
+//     'Contet-Type': 'application/json',
+//   },
+// });
+// interface Task {
+//   id: number;
+//   done: boolean;
+//   title: string;
+//   description: string;
+// }
 
-interface newTask{
-  text: null
-} 
+// @Component
+// export default class App extends Vue {
+//   private task: Task[] = [];
 
-@Component
-export default class App extends Vue {
-  
-  private task: Task[] =  
-  [
-    {
-      done: false,
-      text: "test1"
-      },
-      {
-      done: false,
-      text: "test2"
-    },
-  ]
+//   private newTask = '';
 
-private newTask: Task[] = new Array<Task>()
+//   get completedTasks(): number {
+//     return this.task.filter((task) => task.done).length;
+//   }
+//   get progress(): number {
+//     return (this.completedTasks / this.task.length) * 100;
+//   }
+//   get remainingTasks(): number {
+//     return this.task.length - this.completedTasks;
+//   }
 
-  get completedTasks(): number {
-     return this.task.filter(task => task.done).length
-  }
-  get progress(): number {
-    return this.completedTasks / this.task.length * 100
-  }
-  get remainingTasks(): number {
-    return this.task.length - this.completedTasks
-  }
-   public create() {
-        this.task.push({
-          done: false,
-          text: this.newTask
-        })
-    }
-  }
+//   public create() {
+//     axios.post('http://localhost:3000/todos', {
+//       done: false,
+//       title: this.newTask,
+//       description: '',
+//     });
+//     axios
+//       .get('http://localhost:3000/todos')
+//       .then((res) => {
+//         this.task = res.data;
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//       });
+//     this.newTask = '';
+//   }
+//   public checkDone(id: number, done: boolean) {
+//     axios.patch('http://localhost:3000/todos/' + id, {
+//       id: id,
+//       done: done,
+//     });
+//     // .then(function(res){
+
+//     // })
+//   }
+//   created() {
+//     axios
+//       .get('http://localhost:3000/todos')
+//       .then((res) => {
+//         this.task = res.data;
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//       });
+//   }
+// }
 </script>
